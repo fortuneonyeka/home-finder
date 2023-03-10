295e9a9d301e9867e77165484fad9dd7023c0299
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-03-10 12:57:59 +0100

    property model validation

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,3 +1,9 @@
 class Property < ApplicationRecord
   belongs_to :account
+
+  validates :name ,presence: true
+  validates :address ,presence: true
+  validates :price ,presence: true
+  validates :rooms ,presence: true
+  validates :bathrooms ,presence: true
 end
