d24a9744644a4dc248f3e8b6c9b6b82dceb22a3d
Ihedoro Fortunatus O <ihedfortune@gmail.com>
2023-03-13 11:49:29 +0100

    refactore major UI

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -1,6 +1,6 @@
 class PropertiesController < ApplicationController
   before_action :set_property, only: [ :show, :edit, :update, :destroy ]
-  before_action :authenticate_account!, only: [:new, :create, :update, :destroy]
+  before_action :authenticate_account!, only: [:new, :create, :update, :destroy, :edit]
 
   # GET /properties or /properties.json
   def index
